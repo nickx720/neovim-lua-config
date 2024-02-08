644447a9afb9b84a69fd56914502a6dffcf4bc2f
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>
2024-02-08 09:49:15 -0500

    Initial Commit

---
+++ lua/options.lua
@@ -0,0 +1,69 @@
+
+vim.opt.tabstop = 4
+vim.opt.autoindent = true
+vim.opt.scrolloff=5
+
+-- Auto close
+vim.api.nvim_set_keymap('i', '"<space>', '""<left>', {noremap = true})
+vim.api.nvim_set_keymap('i', "'<space>", "''<left>", {noremap = true})
+vim.api.nvim_set_keymap('i', '`<space>', '``<left>', {noremap = true})
+vim.api.nvim_set_keymap('i', '(<space>', '()<left>', {noremap = true})
+vim.api.nvim_set_keymap('i', '[<space>', '[]<left>', {noremap = true})
+vim.api.nvim_set_keymap('i', '{<space>', '{}<left>', {noremap = true})
+vim.api.nvim_set_keymap('i', '<<CR>', '<><left>', {noremap = true})
+
+-- Set Mouse support
+vim.opt.mouse = "a"
+
+-- Moving selected text up and down
+vim.api.nvim_set_keymap('x', 'K', ":move '<-2<CR>gv-gv", {noremap = true})
+vim.api.nvim_set_keymap('x', 'J', ":move '>+1<CR>gv-gv", {noremap = true})
+
+
+-- Remove trailing whitespace and newlines at file save
+vim.cmd([[
+  autocmd BufWritePre * %s/\s\+$//e
+  autocmd BufWritePre * %s/\n\+\%$//e
+  autocmd BufWritePre *.c,*.h %s/\%$/\r/e
+]])
+
+-- Opens images in neovim
+vim.cmd([[
+  autocmd BufEnter *.png,*.jpg,*.gif exec "! ~/.iterm2/imgcat " .. vim.fn.expand("%") | :bw
+]])
+
+-- Replacing NERDTree with NetRw
+-- Set netrw options
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 4
+vim.g.netrw_altv = 1
+vim.g.netrw_winsize = 20
+vim.g.NetrwIsOpen = 0
+
+-- Define function to toggle netrw
+function ToggleNetrw()
+  if vim.g.NetrwIsOpen == 1 then
+    local i = vim.fn.bufnr("$")
+    while i >= 1 do
+      if vim.fn.getbufvar(i, "&filetype") == "netrw" then
+        vim.cmd("silent bwipeout " .. i)
+      end
+      i = i - 1
+    end
+    vim.g.NetrwIsOpen = 0
+  else
+    vim.g.NetrwIsOpen = 1
+    vim.cmd("silent Lexplore")
+  end
+end
+
+-- Define mapping for <F2> to call ToggleNetrw function
+vim.api.nvim_set_keymap('n', '<F2>', ':lua ToggleNetrw()<CR>', { silent = true })
+
+-- Auto format rust files on exit
+vim.g.rustfmt_autosave = 1
+
+-- Bindings for quick file search
+vim.api.nvim_set_keymap('n', '<C-f>', ':Files<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-A-f>', ':Rg<CR>', {noremap = true})
---
+++ lua/plugins.lua
@@ -0,0 +1,78 @@
+local M = {}
+
+function M.setup()
+  -- Indicate first time installation
+  local packer_bootstrap = false
+
+  -- packer.nvim configuration
+  local conf = {
+    profile = {
+      enable = true,
+      threshold = 1, -- the amount in ms that a plugins load time must be over for it to be included in the profile
+    },
+
+    display = {
+      open_fn = function()
+        return require("packer.util").float { border = "rounded" }
+      end,
+    },
+  }
+
+  -- Check if packer.nvim is installed
+  -- Run PackerCompile if there are changes in this file
+  local function packer_init()
+    local fn = vim.fn
+    local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+    if fn.empty(fn.glob(install_path)) > 0 then
+      packer_bootstrap = fn.system {
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+      }
+      vim.cmd [[packadd packer.nvim]]
+    end
+    vim.cmd "autocmd BufWritePost plugins.lua source <afile> | PackerCompile"
+  end
+
+  -- Plugins
+  local function plugins(use)
+    use { "wbthomason/packer.nvim" }
+
+    -- Load only when require
+    use { "nvim-lua/plenary.nvim", module = "plenary" }
+
+    -- Vim Git
+    use {
+      "tpope/vim-fugitive",
+      cmd = { "Git", "GBrowse", "Gdiffsplit", "Gvdiffsplit" },
+    }
+
+	-- DAP Client For Debugging
+	use 'mfussenegger/nvim-dap'
+
+	-- FZF Installer
+	use {
+    'junegunn/fzf.vim',
+    requires = { 'junegunn/fzf', run = ':call fzf#install()' }
+ }
+
+
+
+    -- Bootstrap Neovim
+    if packer_bootstrap then
+      print "Restart Neovim required after installation!"
+      require("packer").sync()
+    end
+  end
+
+  -- Init and start packer
+  packer_init()
+  local packer = require "packer"
+  packer.init(conf)
+  packer.startup(plugins)
+end
+
+return M
