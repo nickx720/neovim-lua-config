1ce9e34cd595dc07347e64e5aa9f79a4aea3287b
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>
2024-07-19 02:10:44 -0400

    Adding support for gitgrep

--- lua/options.lua
+++ lua/options.lua
@@ -89,3 +89,11 @@ vim.g.loaded_ruby_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_node_provider = 0
+
+-- Vim Fugitive Settings
+vim.api.nvim_create_autocmd("QuickFixCmdPost", {
+  pattern = "*grep*",
+  callback = function()
+    vim.cmd("cwindow")
+  end,
+})
