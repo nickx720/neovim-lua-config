def30289d0dd36cbf284949daf3c5fcbb0a5b6dc
Nick <nicktomthomas@gmail.com>
2026-03-31 04:39:04 -0400

    chore: auto loading parser

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -6,6 +6,13 @@ function M.setup()
   -- Install parsers
   require('nvim-treesitter').install(parsers)
 
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = parsers,
+    callback = function(args)
+      pcall(vim.treesitter.start, args.buf)
+    end,
+  })
+
   vim.opt.foldmethod = 'manual'
 end
 
